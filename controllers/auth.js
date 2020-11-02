d3581f561cf44606a24e49e985c0e3574960d7b2
MarkoWilliam <yowanwilliam@gmail.com>
2020-11-02 15:29:06 +0300

    Insertion user ok mais message d'erreur n'est pas encore valider

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
 const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 //--Connection avec la base de donnée--
 const db = mysql.createConnection({
@@ -15,22 +18,40 @@ exports.register = (req, res) => {
 
     const { nom, email, password } = req.body;
 
-    db.query('SELECT email FROM kenza user WHERE email = ?', [email], (error, results) => {
+    db.query('SELECT email FROM user user WHERE email = ?', [email], async(error, results) => {
         if (error) {
             console.log(error);
         }
 
         if (results.length > 0) {
-            return res.render('register', {
-                message: 'That email is already in use'
-            })
+            // return res.render('', {
+            //     message: 'That email is already in use'
+            // })
+            res.send("That email is already in use");
         }
         //  else if(password !== passwordconfirm) {
         //     return res.render('register', {
         //         message: 'Passwords do not math'
         //     });
         // }
-    })
 
-    res.send("From submitted");
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword)
+
+        //-----------INSERTION dans la base de données-------------------
+        db.query('INSERT INTO user SET ?', { nom: nom, email: email, password: hashedPassword }, (error, results) => {
+            if (results) {
+                console.log(results);
+                res.send("user registered");
+            } else {
+                console.log(error);
+                // return res.render('', {
+                //     message: 'user registered'
+                // });
+                res.send("Register user failed");
+            }
+        })
+    });
+
+    // res.send("Test");
 }
