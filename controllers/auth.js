e45ed293f75f0c9dd106bba63d4d9a829dec39af
MarkoWilliam <yowanwilliam@gmail.com>
2020-11-13 14:27:32 +0300

    système d'erreur ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
-// const { encryptPWD, comparePWD } = require('../helpers');
 
 // module.exports = authorize;
 //--Connection avec la base de donnée--
@@ -18,34 +17,24 @@ const db = mysql.createConnection({
 exports.login = (req, res) => {
     try {
         // passport.authenticate('local');
-        const { email, password } = req.body;
-        console.log("Password", password)
 
-        // if (!email || !password) {
-        //     return res.status(400).json('Please provide an email and password')
-        // }
+        const { email, password } = req.body;
+        console.log("REq body", req.body)
 
         var headers = {};
         // IE8 n'autorise pas la spécification de domaines, juste le *
         // headers ["Access-Control-Allow-Origin"] = req.headers.origin;
+        if (req.body.password === '' || req.body.email === '') {
+            console.log("Test 1")
+            res.status(405).json({
+                message: 'Aucun résultat'
+            });
+        }
 
 
 
-        db.query('SELECT * FROM user WHERE email = ?', [email], async(error, results) => {
+        db.query('SELECT * FROM user WHERE email = ?   ', [email], async(error, results) => {
             console.log("ito", results);
-            // let hashedPassword = await bcrypt.hash(password, 8);
-            // console.log("crypter", hashedPassword);
-            // console.log("crypter 1", hashedPassword);
-            // const salt = await bcrypt.genSaltSync(10);
-            // const password = await req.body.password;
-            //-------------Il y a une problème sur cette ligne code-----------------
-            // if (!results || results[0].password !== password) {
-
-            //-------------Solution n'est pas fiable-----------------
-            // if (!results || !(await bcrypt.compare(password, hashedPassword))) {
-
-            console.log("Result test", results[0].password);
-
             if (req.method === 'OPTIONS') {
                 console.log('! OPTIONS');
                 var headers = {};
@@ -58,30 +47,33 @@ exports.login = (req, res) => {
                 headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
                 res.writeHead(200, en - têtes);
                 res.end();
-            } else if (!results || !(await bcrypt.compare(password, results[0].password))) {
 
-                //---------cette ligne marche si le mot de passe n'est pas crypter----------
-                // if (!results || password != results[0].password) {
+            } else if (email != results.email) {
+                console.log("Test 2")
+                res.status(406).json({
+                    message: 'Aucun résultat'
+                });
+            } else if (!results || !(bcrypt.compareSync(password, results[0].password))) {
                 res.status(401).json({
-                        message: 'Email or Password id incorrect'
-                    })
-                    // res.send("Email or Password id incorrect");
-            } else {
-                const id = results[0].id;
-
-                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
+                    message: 'Email or Password id incorrect'
                 });
 
-                console.log("The token is:" + token);
+            } else {
+                // const id = results[0].id;
 
-                const cookieOptions = {
-                    expires: new DataCue(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-                    ),
-                    httpOnly: true
-                }
-                res.cookie('jwt', token, cookieOptions);
+                // const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                //     expiresIn: process.env.JWT_EXPIRES_IN
+                // });
+
+                // console.log("The token is:" + token);
+
+                // const cookieOptions = {
+                //     expires: new DataCue(
+                //         Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                //     ),
+                //     httpOnly: true
+                // }
+                // res.cookie('jwt', token, cookieOptions);
 
                 // ---------------Valeur à retourner-----------------
                 res.json(results[0]);
@@ -92,37 +84,11 @@ exports.login = (req, res) => {
             //--------------------------------------------
         })
 
+
     } catch (err) {
         console.log(err);
     }
 
-    //     db.query('SELECT * FROM user WHERE email = ?', [email], async(err, results) => {
-
-    //         if (results.length < 1) {
-    //             return res.status(401).render({
-    //                 message: 'Enter valid email or password'
-    //             });
-    //         } else if (!(await bcrypt.compare(password, results[0].password))) {
-
-    //             return res.status(401).render({
-    //                 message: 'Enter valid email or password'
-
-    //             });
-
-    //         } else {
-
-    //             req.session.email = req.body.email;
-    //             var email = req.session.email;
-
-    //             // res.status(200).redirect('/home')  
-    //             res.json(results[0]);
-
-    //         }
-    //     });
-
-    // } catch (err) {
-    //     console.log(err);
-    // }
 
 
 }
@@ -134,9 +100,9 @@ exports.register = (req, res) => {
     const { nom, email, password } = req.body;
 
     db.query('SELECT email FROM user user WHERE email = ?', [email], async(error, results) => {
-        if (error) {
-            console.log("L'inscription réfuer", error);
-        }
+        // if (error) {
+        //     console.log("L'inscription réfuer", error);
+        // }
 
         if (results.length > 0) {
             // Cette fonction retourne la view register avec le message d'erreur
@@ -146,11 +112,19 @@ exports.register = (req, res) => {
             })
         }
 
-        let hashedPassword = await bcrypt.hash(password, 8);
-        console.log(hashedPassword)
+        // let hashedPassword = await bcrypt.hash(password, 8);
+
+        // const encryptPWD = (password) => {
+        //     // Hash password and salt with md5 encryption
+        //     return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+        // }
+
+        let encryptPWD = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+        // console.log(hashedPassword)
 
         //-----------INSERTION dans la base de données-------------------
-        db.query('INSERT INTO user SET ?', { nom: nom, email: email, password: hashedPassword }, (error, results) => {
+        db.query('INSERT INTO user SET ?', { nom: nom, email: email, password: encryptPWD }, (error, results) => {
             if (results) {
                 console.log("Inscription results", results);
                 // res.send("user registered");
@@ -164,7 +138,6 @@ exports.register = (req, res) => {
                 //     message: 'user registered'
                 // });
                 return res.json(results);
-                // res.send("Register user failed");
             }
         })
     });
--- helpers/helper.js
+++ helpers/helper.js
@@ -1,14 +1,11 @@
-const bcrypt = require('bcryptjs');
-
-// Password encryption Function
-const encryptPWD = (password) => {
-    // Hash password and salt with md5 encryption
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-};
-// Password comparison Functon
-const comparePWD = (password1, password2) => {
-    // Compare two passwords
-    return bcrypt.compareSync(password1, password2);
-};
-// Export module
-module.exports = { encryptPWD, comparePWD };
+//  // Password encryption Function
+//  const encryptPWD = (password) => {
+//      // Hash password and salt with md5 encryption
+//      return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+//  };
+//  // Password comparison Functon
+//  const comparePWD = (password1, password2) => {
+//      // Compare two passwords
+//      return bcrypt.compareSync(password1, password2);
+//  };
+//  // Export module
